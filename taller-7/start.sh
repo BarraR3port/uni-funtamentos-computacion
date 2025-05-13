5492f06863d2a4f6ebda9743d00fde3ea514b641
BarraR3port <barrar3port@gmail.com>
2025-05-12 22:25:27 -0400

    Agregar mensajes de compilación y ejecución al script de inicio para mejorar la retroalimentación del usuario durante el proceso de ejecución de la aplicación.

--- taller-7/start.sh
+++ taller-7/start.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
+echo "Compilando..."
 javac -d target/classes src/main/java/cl/unab/fundamentos/*.java src/main/java/cl/unab/fundamentos/*/*.java
+echo "Ejecutando..."
 java -cp target/classes cl.unab.fundamentos.Main
